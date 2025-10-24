40039769cb32a367dc6eab1d4f0538329db6891a
Ailen Fuentes <ailengfuentes@gmail.com>
2025-10-24 20:38:56 -0300

    ult cambios

--- src/test/test_login_page.py
+++ src/test/test_login_page.py
@@ -1,8 +1,6 @@
-import pytest
+
 from src.pages.BasePage import BasePage
 from src.pages.LoginPage import LoginPage
-from selenium.webdriver.common.by import By
-
 
 class TestLogin:
 
@@ -22,16 +20,3 @@ class TestLogin:
         products_text = self.LoginPage.get_text(self.LoginPage.product_selector)
 
         assert "Products" in products_text , f"Se esperaba 'Products' en el texto, pero se obtuvo: {products_text}"
-
-    def test_url_valida(self):
-        self.LoginPage.login_completo()
-
-        current_url = self.LoginPage.login_data["url"]
-
-
-        assert "https://www.saucedemo.com/inventory.html" == current_url
-
-
-    #def test_login_fixture(login_fixture): #aca uso al fixture como precondicion, por ende hace todo el login, debo usarlo a futuro
-        #login_page = login_fixture
-
